15390f05a5d6ca802a6d87c24f7f414192b98323
Boston Jones <bluaie1920@gmail.com>
2024-10-27 23:15:46 -0500

    did basic homepage and profile page for StatSnap

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
